3f24a9a2b898e0eab00516e39bf9c46acf3f1591
나연 <kny5579@naver.com>
2025-04-22 17:29:18 +0900

    init

---
+++ src/main/java/com/couponmoa/backend/couponmoagateway/CouponmoaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponmoaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponmoaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/couponmoa/backend/couponmoagateway/CouponmoaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponmoaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
